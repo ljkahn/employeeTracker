92ea7f9c9f78f5d0d6f66a793b2d30e0b83d940c
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-29 09:48:42 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-const mysql = require('mysql2');
+
 const inquirer = require('inquirer');
-const PORT = 2001;
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'KJK1LJK2!',
-  database: 'company_db'
-},
-  console.log(`Connected to the company_db database!`)
+const config = require('./config/connection');
+const util = require('util');
+const query = util.promisify(config.query).bind(config);
 
-);
+const initQuestion = [
+  {
+    type: "list",
+    name: "choice",
+    message: "Please choose!",
+    choices: [
+      "View all department.",
+      "View all role.",
+      "View all employees.",
+      "Add a department.",
+      "Add a role.",
+      "Add an employee.",
+      "Update an employee role.",
+      "Quit.",
+    ],
+  },
+];
 
-
-// //GET ALL MOVIES FROM DATABASE TABLE  
-
-// app.get('/api/departments', async (req, res) => {
-//   console.log(req);
-//   //now i want to interact with the db
-//   const movieResponse = await query('SELECT * FROM departments', (err, data) => {
-//     res.json(movieResponse);
-//     console.log(data);
-//   })
-// })
-
-
-// app.get('/api/movie-reviews', async (req, res) => {
-//   console.log(req);
-//   //now i want to interact with the db
-//   const movieReviewResponse = await query('SELECT * FROM employees', (err, data) => {
-//     res.json(movieReviewResponse);
-//     console.log(data);
-//   })
-// })
-
-
-// //POST REQUESTS
-
-// app.post('/api/add-movie', (req, res) => {
-//   console.log(res)
-// })
-
-
-
-
-app.listen(PORT, () => {
-  console.log('API server is listening on ${PORT}')
-});
-
-
-function init() {
-  //inquirer prompt for questions
-  inquirer.prompt(questions)
-    .then((data) => {
-      //validation that it doesnt accept more than 3 characters
-      if (data.text.length > 3) {
-        return console.error("You must use a maximum of three characters. Please try again.");
-
-      }
-    }
-    )
+async function init() {
+  const { choice } = await inquirer.prompt(initQuestion);
+  console.log(choice);
+  switch (choice) {
+    case "View all department.":
+      viewAlldepartment();
+      break;
+    case "View all role.":
+      viewAllRole();
+      break;
+    case "View all employees.":
+      viewAllEmployees();
+      break;
+    case "Add a department.":
+      addADepartment();
+      break;
+    case "Add a role.":
+      addARole();
+      break;
+    case "Add an employee.":
+      addAEmployee();
+      break;
+    case "Update an employee role.":
+      updateEmployeeRole();
+      break;
+    case "Quit.":
+      quit();
+      break;
+    default:
+  }
+  return;
 }
+
+async function viewAlldepartment() {
+  const result = await query("SELECT * FROM department");
+  console.table(result);
+  init();
+}
+
+//create an async function to initialize the application
+
+
+//use switch statement to perform different actions based on user selection
