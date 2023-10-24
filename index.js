b5f3f2749c2ab57cdd9af81df4ec5433cde2ab36
ljkahn <lia.j.kahn@gmail.com>
2023-10-24 11:53:21 -0600

    created function for employee info and store responses

--- index.js
+++ index.js
@@ -100,7 +100,13 @@ const questions = [
     message: `Enter ID number of employee's manager:`,
     choices: manager,
   },
-
-
   
-]
+];
+
+const { first_name, last_name, role_id, manger_id } = await inquirer.prompt(questions);
+
+
+await query(
+  `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+  [first_name, last_name, role_id, manager_id]
+);
