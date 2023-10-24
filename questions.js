b73fa1b224a67e8c9df37db0ff10d530d26859e5
ljkahn <lia.j.kahn@gmail.com>
2023-10-24 10:46:30 -0600

    updated questions.js

---
+++ questions.js
@@ -0,0 +1,48 @@
+module.exports = {
+    //create a list of options for the menu
+    createList: () => {
+        const list = [
+            {
+                name: 'View All Employees',
+                value: 1
+            },
+
+                     {
+                name: 'Add Employee',
+                value: 2
+            },
+
+                     {
+                name: 'Update Employee Role',
+                value: 3
+            },
+
+                     {
+                name: 'View All Roles',
+                value: 4
+            },
+
+                     {
+                name: 'Add Role',
+                value: 5
+            },
+
+                     {
+                name: 'View All Departments',
+                value: 6
+            },
+
+                     {
+                name: 'Add Department',
+                value: 7
+            },
+
+                     {
+                name: 'Quit',
+                value: 8
+            },
+
+        ]
+    }
+
+}
